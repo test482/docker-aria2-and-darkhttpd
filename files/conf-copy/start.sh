6a8a31891bc875de6367515bf37c03da19874d70
root <root@aliyun.localdomain>
2019-12-12 21:06:12 +0800

    [Update] update config file
    Thanks to P3TERX/aria2_perfect_config

--- files/conf-copy/start.sh
+++ files/conf-copy/start.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+
 if [ ! -f /conf/aria2.conf ]; then
 	cp /conf-copy/aria2.conf /conf/aria2.conf
 	if [ ${SECRET} ]; then
 		echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
 	fi
 fi
+
 if [ ! -f /conf/on-complete.sh ]; then
 	cp /conf-copy/on-complete.sh /conf/on-complete.sh
 fi
