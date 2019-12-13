4fdaf8ad67b6d80ed5163d92050ad251ae58120d
test482 <eliotjoking@gmail.com>
2019-12-13 13:00:11 +0800

    [Update] touch dht.dat if not exist

--- files/conf-copy/start.sh
+++ files/conf-copy/start.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 if [ ! -f /conf/aria2.conf ]; then
-	cp /conf-copy/aria2.conf /conf/aria2.conf
-	if [ ${SECRET} ]; then
-		echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
-	fi
+    cp /conf-copy/aria2.conf /conf/aria2.conf
+    if [ ${SECRET} ]; then
+        echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
+    fi
+fi
+
+if [ ! -f /conf/dht.dat ]; then
+    touch /conf/dht.dat
 fi
 
 if [ ! -f /conf/on-complete.sh ]; then
-	cp /conf-copy/on-complete.sh /conf/on-complete.sh
+    cp /conf-copy/on-complete.sh /conf/on-complete.sh
 fi
 
 chmod +x /conf/on-complete.sh
@@ -17,9 +21,9 @@ touch /conf/aria2.session
 darkhttpd /webui-aria2/docs --port 80 &
 if [ ${AUTH_USERNAME} -a ${AUTH_PASSWD} ]; 
 then
-	darkhttpd /data --port 8080 --auth ${AUTH_USERNAME}:${AUTH_PASSWD} &
+    darkhttpd /data --port 8080 --auth ${AUTH_USERNAME}:${AUTH_PASSWD} &
 else
-	darkhttpd /data --port 8080 &
+    darkhttpd /data --port 8080 &
 fi
 
 aria2c --conf-path=/conf/aria2.conf
